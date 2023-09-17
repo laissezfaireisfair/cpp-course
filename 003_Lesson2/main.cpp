c65e318a3cab6f3480272b3e079a30105cc4034e
Andrey Golubev <laissezfaireisfair@gmail.com>
2023-09-17 19:38:20 +0700

    Fix lesson 2 task (phone check)

--- 003_Lesson2/main.cpp
+++ 003_Lesson2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <regex>
 
-bool check_braces_and_remove(std::string const &string) {
+bool check_braces(std::string const& string) {
   int leftBracesCount = 0;
   int rightBracesCount = 0;
 
@@ -22,16 +22,19 @@ bool check_braces_and_remove(std::string const &string) {
   return leftBracesCount == rightBracesCount;
 }
 
-std::string try_parse_number(std::regex regex, std::string &&raw) {
-  auto areBracesOk = check_braces_and_remove(raw);
+std::string try_parse_number(std::regex const & regex, std::regex const & badPattern, std::string && raw) {
+  auto areBracesOk = check_braces(raw);
   if (!areBracesOk)
-    return std::string();
-  auto filtered = std::ranges::fi
+    return {};
 
   std::smatch match;
   auto isFound = std::regex_match(raw, match, regex);
   if (!isFound)
-    return std::string();
+    return {};
+
+  auto isContainBadPattern = std::regex_search(raw, badPattern);
+  if (isContainBadPattern)
+    return {};
 
   std::string formatted("+7");
   bool isFirstSkipped = false;
@@ -44,16 +47,16 @@ std::string try_parse_number(std::regex regex, std::string &&raw) {
     }
   }
 
-  return std::string();
+  return formatted;
 }
 
 int main() {
   std::string input;
   std::getline(std::cin, input);
 
-  static const std::regex regex(
-      "((\\+ *7)|8)-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d-? *-?\\d");
-  auto parsed = try_parse_number(regex, std::move(input));
+  static const std::regex regex(R"rgx(((\+ *7)|8)([- ()]*\d){10})rgx");
+  static const std::regex badPattern(R"rgx(--)rgx");
+  auto parsed = try_parse_number(regex, badPattern, std::move(input));
 
   if (parsed.empty())
     std::cout << "error" << std::endl;
